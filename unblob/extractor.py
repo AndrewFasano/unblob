9f6284c4c52725d114d9ab394a7e8d9845c0b6ef
Andrew Fasano <fasano@mit.edu>
2024-02-10 17:02:43 -0500

    Use symlinks(8) to handle symlinks

--- unblob/extractor.py
+++ unblob/extractor.py
@@ -3,6 +3,7 @@ import errno
 import os
 from pathlib import Path
 from typing import Union
+from subprocess import check_output
 
 from structlog import get_logger
 
@@ -21,131 +22,27 @@ def carve_chunk_to_file(carve_path: Path, file: File, chunk: Chunk):
     logger.debug("Carving chunk", path=carve_path)
     carve(carve_path, file, chunk.start_offset, chunk.size)
 
-
-def fix_permission(path: Path):
-    if not path.exists():
-        return
-
-    if path.is_symlink():
-        return
-
-    mode = path.stat().st_mode
-
-    if path.is_file():
-        mode |= FILE_PERMISSION_MASK
-    elif path.is_dir():
-        mode |= DIR_PERMISSION_MASK
-
-    path.chmod(mode)
-
-def is_recursive_link(path: Path) -> bool:
-    """Check if the symlink creates a loop."""
-    try:
-        path.resolve(strict=True)
-    except RecursionError:
-        return True
-    return False
-
-def safe_resolve(path: Path, root_dir: Path) -> Path:
-    try:
-        return path.resolve()
-    except RuntimeError as e:
-        logger.error(f"Symlink loop detected for {path}: {e}")
-        # Return the path as is or handle it according to your needs
-        return path
-
-def is_potential_loop(target: Path, root_dir: Path) -> bool:
-    resolved_target = target.resolve()
-    return resolved_target.is_symlink() and resolved_target.resolve() == target
-
-def fix_symlink(path: Path, root_dir: Path, task_result : TaskResult) -> Path:
-    '''
-    Make a symlink relative to root_dir.
-    '''
-    assert(path.is_symlink())
-
-    target = path.readlink()
-
-    if Path(target).is_absolute():
-        # Convert absolute target to be relative to root_dir
-        absolute_target = root_dir / str(target).lstrip('/')
-    else:
-        # Resolve relative target against symlink's directory
-        absolute_target = safe_resolve(path.parent / target, root_dir)
-        logger.debug(f"Target {target} is relative, resolved to {absolute_target}")
-
-    # Check if the absolute target exists within root_dir. Also check that it's not a dangling symlink
-    if not absolute_target.exists() and not os.path.lexists(absolute_target):
-        logger.warning(f"Symlink {path} -> {absolute_target} but target does not exist. Creating a placeholder and linking to it.")
-
-        # We may need to create 1 or more directories before we can touch absolute_target
-        # track the directories we need to create
-        dirs_to_create = []
-        parent = absolute_target.parent
-        while parent and not parent.exists():
-            dirs_to_create.append(parent)
-            parent = parent.parent
-        if len(dirs_to_create):
-            # Just create all the directories we need at once
-            absolute_target.parent.mkdir(parents=True, exist_ok=True)
-        # Create the placeholder file
-        absolute_target.touch()
-
-        # Calculate new target relative to the symlink's directory
-        new_target = os.path.relpath(absolute_target, start=path.parent)
-        # Replace the symlink with the new target
-        path.unlink()
-
-        if is_potential_loop(path, new_target):
-            logger.error(f"Potential symlink loop detected for {path} -> {new_target}. Skipping modification.")
-            return path
-
-        path.symlink_to(new_target)
-        logger.info(f"Symlink at {path} now points to {new_target}.")
-
-        # Remove the placeholder to intentionally leave a dangling symlink.
-        # Also remove directories we created for it
-        absolute_target.unlink()
-        for d in dirs_to_create:
-            os.rmdir(d)
-
-        logger.info(f"Removed placeholder {absolute_target}, leaving a dangling symlink at {path}->{absolute_target}.")
-        # Assert that the symlink is now dangling
-        assert(os.path.islink(path) and not os.path.exists(os.readlink(path)))
-
-    else:
-        # If the target exists, simply adjust the symlink as necessary
-        new_target = os.path.relpath(absolute_target, start=path.parent)
-        path.unlink()
-
-        if is_potential_loop(path, new_target):
-            logger.error(f"Potential symlink loop detected v2 for {path} -> {new_target}. Skipping modification.")
-            return path
-
-        path.symlink_to(new_target)
-        logger.debug(f"Symlink at {path} updated to point to {new_target}.")
-
-    return path
-
 def fix_extracted_directory(outdir: Path, task_result: TaskResult):
-    def _fix_extracted_directory(directory: Path):
-        if not directory.exists():
-            return
-        for path in (directory / p for p in os.listdir(directory)):
-            try:
-                fix_permission(path)
-                if path.is_symlink():
-                    fix_symlink(path, outdir, task_result)
-                    continue
-                if path.is_dir():
-                    _fix_extracted_directory(path)
-            except OSError as e:
-                if e.errno == errno.ENAMETOOLONG:
-                    continue
-                raise e from None
+    '''
+    Given an extracted directly, we want to make sure there are
+    no symlinks that point to absolute paths outside of our extraction.
 
-    fix_permission(outdir)
-    _fix_extracted_directory(outdir)
+    We use the symlinks(8) utility to replace all absolute symlinks
+    with relative symlinks recursively within the directory and
+    ensure all symlinks are within the directory.
+    '''
+
+    if not outdir.exists():
+        return
+
+    try:
+        check_output(["symlinks", "-cr", str(outdir)])
+    except Exception as e:
+        logger.error(f"Failed to run symlinks on {outdir}: {e}")
+        # We should not continue if symlinks fails as our directory
+        # may have now contain symlinks that point outside outdir
+        raise e
+
 
 
 def carve_unknown_chunk(
